c179335c3900a490ed5f0214fdf65aeb695d8080
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-10-22 12:53:59 +0200

    added another function

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 const showElement = document.getElementById('show');
-const btnShow = document.getElementById('btnShow');
-const showImg = document.getElementById('showImg');
-const description = document.getElementById('description');
-const cont = document.getElementById('container');
+const btnComedy = document.getElementById('btnComedy');
+const btnShowHorror = document.getElementById('btnShowHorror');
 
-async function getShows() {
+const cont = document.getElementById('comedy-container');
+
+async function getComedyShow() {
     try {
         const url = "https://api.tvmaze.com/search/shows?q=comedy";
         const res = await fetch(url);
@@ -13,35 +13,50 @@ async function getShows() {
         const data = await res.json();
         console.log(data);
 
-
-
         data.forEach(item => {
-            const show = item.show;
-            const div = document.createElement('div');
 
+            const showName = document.createElement('h3');
+            const showImg = document.createElement('img');
+            const description = document.createElement('p');
+
+            const div = document.createElement('div');
             div.classList.add('showCard');
 
-            const imgSrc = show.image ? show.image.medium : "Cannot show img";
-            const summary = show.summary ? show.summary : "no desc";
+            showImg.src = item.show.image ? item.show.image.medium : "Cannot show img";
+            description.textContent = item.show.summary ? item.show.summary : "no desc";
+            showName.textContent = item.show.name ? item.show.name : "No name";
 
-
-
-
-            div.innerHTML = `
-                <img src="${imgSrc}" alt="${show.name}">
-                <h4>${show.name}</h4>
-                <p>${summary}</p>
-
-            `;
-
-            // div.innerHTML = `
-            // <img src="${item.show.image ? item.show.image.medium : ''}" alt="${item.show.name}">
-            // <p>${item.show.summary || 'No description available.'}</p>
-            // `;
-            cont.appendChild(div);
+            cont.append(showImg, description, showName);
         });
+    } catch (err) {
+        console.error('Couldnt fetch data', err.message);
+    }
+}
+
+async function getHorrorShow() {
+    try {
+        const url = "https://api.tvmaze.com/search/shows?q=horror";
+        const res = await fetch(url);
+        if (!res.ok) throw Error('Couldnt fetch horror');
+        const data = await res.json();
+
+        console.log(data);
+
+        data.forEach((item) => {
+            const showImg = document.createElement('img');
+            const showName = document.createElement('h3');
+            const showDescription = document.createElement('p');
+
+            showImg.src = item.show.image ? item.show.image.medium : "No image available";
+            showName.textContent = item.show.name ? item.show.name : "No image available";
+            showDescription.textContent = item.show.description ? item.show.description : "No description available";
 
 
+            cont.append(showImg, showName, showDescription);
+
+
+
+        });
     } catch (err) {
         console.error('Couldnt fetch data', err.message);
     }
@@ -49,6 +64,10 @@ async function getShows() {
 
 
 
-btnShow.addEventListener('click', () => {
-    getShows();
+btnComedy.addEventListener('click', () => {
+    getComedyShow();
+});
+
+btnShowHorror.addEventListener('click', () => {
+    getHorrorShow();
 });
